66c8c85bdf78df1f976e9293d2d633034c8c66df
Jordan <jordanfhoughton@gmail.com>
2024-10-22 16:58:36 +0100

    header changes

--- rogue-coffee-co/src/app/components/header/header.tsx
+++ rogue-coffee-co/src/app/components/header/header.tsx
@@ -3,33 +3,49 @@
 import React, {useState} from "react";
 import Image from "next/image";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import { VscChromeClose } from "react-icons/vsc";
 import styles from "./header.module.css";
-
 
 export default function Header() {
     
     const [menuOpen, setMenuOpen] = useState(false);
-
+    
+    const toggleMenu = () => { 
+        setMenuOpen(!menuOpen);
+    }
+    
   return (
 <header className="header">
     <div className={styles.headerContainer}>
+
+    {menuOpen && (
+        <nav className={styles.menu}>
+          <button className="close-menu" onClick={toggleMenu}>
+          <VscChromeClose />
+          </button>
+          <ul className={styles.list}>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      )}
+        
         <div className={styles.headerLogo}>
         <a href="/">
             <Image src="/images/basic-removebg-preview.png" alt="Rogue Coffee Co." width={100} height={100}/>
         </a>
         </div>
-        <nav className="header__nav">
-        <GiHamburgerMenu className={styles.burger} onClick={() => setMenuOpen(true)} />
-        <ul>
-            <li>
-            <a href="/about">About</a>
-            </li>
-            <li>
-            <a href="/contact">Contact</a>
-            </li>
-        </ul>
-        </nav>
+        {!menuOpen && (
+        <button className={styles.burger} onClick={toggleMenu}><GiHamburgerMenu /></button>
+        )}
+        
+        
+
+<div className={styles.headerTitle}>
+            <h1>Rogue Coffee Co.</h1>
+            </div>
+        
     </div>
 </header>
   );
